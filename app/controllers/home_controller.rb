da6e4c54e85dcde4893984bbae8666f73839c841
FroZz <mafiondres12@gmail.com>
2021-07-29 13:37:47 -0700

    subiendo heroku

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+    #@tweets = Tweet.all
+    @tweets = Tweet.order(created_at: :desc).page params[:page]
+    @tweet = Tweet.new
+  end
+  
+end
