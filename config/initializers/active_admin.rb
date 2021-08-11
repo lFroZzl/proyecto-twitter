8d798bd6f7b3d5b0ebca25a069a6cda772d533a7
FroZz <mafiondres12@gmail.com>
2021-08-11 15:17:06 -0700

    cambio ruta Twitter en administrador

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -9,7 +9,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-   config.site_title_link = "https://proyectotwitter1.herokuapp.com"
+   config.site_title_link = "http://localhost:3000"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
